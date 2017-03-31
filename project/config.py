350513c31b13203ee0b1cf7564bd40b33f3fd001
ibrobabs <babskolawole@gmail.com>
2017-03-31 05:34:22 -0600

    tasks

---
+++ project/config.py
@@ -0,0 +1,15 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'taskr.db'
+CSRF_ENABLED = True
+SECRET_KEY = ""
+
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+DEBUG = False
