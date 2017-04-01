a1df0ba1f83995df93fc91f9ab6943a9da9c4de5
ibrobabs <babskolawole@gmail.com>
2017-03-31 23:26:46 -0600

    added tests

--- project/test_main.py
+++ project/test_main.py
@@ -51,7 +51,11 @@ class MainTests(unittest.TestCase):
 			self.assertEquals(response.status_code, 500)
 		except ValueError:
 			pass
-		
+
+	def test_index(self):
+		""" Ensure flask was set up correctly. """
+		response = self.app.get('/', content_type='html/text')
+		self.assertEqual(response.status_code, 200)
 
 
 
